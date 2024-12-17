d9d3f8caebbe4fdc259c5a7000dea5b4a5a33cce
juanCamilo0509 <juank109845@gmail.com>
2024-12-17 17:45:51 -0500

    Add borders to the cmp windows(doc and completion)

--- lua/plugins/autocomplete.lua
+++ lua/plugins/autocomplete.lua
@@ -1,27 +1,38 @@
 local M = {
-	"hrsh7th/nvim-cmp",
-	dependencies = {
-		"hrsh7th/cmp-nvim-lsp",
-		"hrsh7th/cmp-nvim-lua",
-	},
+  "hrsh7th/nvim-cmp",
+  dependencies = {
+    "hrsh7th/cmp-nvim-lsp",
+    "hrsh7th/cmp-nvim-lua",
+  },
 }
 M.config = function()
-	local cmp = require("cmp")
-	vim.opt.completeopt = { "menu", "menuone" }
+  local cmp = require("cmp")
+  vim.opt.completeopt = { "menu", "menuone" }
 
-	cmp.setup({
-		mapping = cmp.mapping.preset.insert({
-			["<C-b>"] = cmp.mapping.scroll_docs(-4),
-			["<C-f>"] = cmp.mapping.scroll_docs(4),
-			["<C-Space>"] = cmp.mapping.complete(),
-			["<C-e>"] = cmp.mapping.abort(),
-			["<CR>"] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
-		}),
-		sources = cmp.config.sources({
-			{ name = "nvim_lsp" },
-		}
-		),
-	})
+  cmp.setup({
+    window = {
+      documentation = {
+        border = { '┌', '─', '┐', '│', '┘', '─', '└', '│' },
+        winhighlight = 'Normal:CmpPmenu,FloatBorder:CmpPmenuBorder,CursorLine:PmenuSel,Search:None',
+
+      },
+      completion = {
+        border = { '┌', '─', '┐', '│', '┘', '─', '└', '│' },
+        winhighlight = 'Normal:CmpPmenu,FloatBorder:CmpPmenuBorder,CursorLine:PmenuSel,Search:None',
+      }
+    },
+    mapping = cmp.mapping.preset.insert({
+      ["<C-b>"] = cmp.mapping.scroll_docs(-4),
+      ["<C-f>"] = cmp.mapping.scroll_docs(4),
+      ["<C-Space>"] = cmp.mapping.complete(),
+      ["<C-e>"] = cmp.mapping.abort(),
+      ["<CR>"] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+    }),
+    sources = cmp.config.sources({
+      { name = "nvim_lsp" },
+    }
+    ),
+  })
 end
 
 return M
