b9363f80963811671629abba8049643b49f2cb10
juanCamilo0509 <juank109845@gmail.com>
2025-04-30 17:15:42 -0500

    Adding mini splitjoin and configure mini diff.

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -14,5 +14,14 @@ return {
 		config = function()
 			require('mini.diff').setup()
 		end
+	},
+	{'echasnovski/mini.splitjoin',
+		config = function()
+    require('mini.splitjoin').setup()
+    local function map(m, k, v)
+      vim.keymap.set(m, k, v, { silent = false })
+    end
+    map('n', "ghs", ':lua MiniDiff.toggle_overlay()<CR>')
+		end
 	}
 }
