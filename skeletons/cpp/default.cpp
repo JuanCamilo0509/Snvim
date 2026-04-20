b59997aa98f49075aa14d9ba293da6a2fc9ff5b1
juanCamilo0509 <juank109845@gmail.com>
2026-04-20 17:13:12 -0500

    Changing from neocmake to cmake-language-server.

---
+++ skeletons/cpp/default.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+/**
+ * Created by ${user} on ${host}
+ * Date: ${date} ${time}
+ */
+
+int main(int argc, char* argv[]) {
+    std::cout << "Hello from ${filename}!" << std::endl;
+    return 0;
+}
---
+++ skeletons/cpp/header.hpp
@@ -0,0 +1,17 @@
+#ifndef ${upper_name}_HPP
+#define ${upper_name}_HPP
+
+/**
+ * @file ${filename}.${fileext}
+ * @author ${gh-user} (${gh-email})
+ * @date ${date}
+ */
+
+class ${filename} {
+public:
+    ${filename}();
+    ~${filename}();
+};
+
+#endif // ${upper_name}_HPP
+
